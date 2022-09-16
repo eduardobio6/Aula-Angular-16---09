4e090ed186a2a024465000a560ee0149dfbf229c
eduardobio6 <101075205+eduardobio6@users.noreply.github.com>
2022-09-16 19:50:09 -0300

    3 files modified

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -3,14 +3,21 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent implements OnInit {
   @Input() name: string;
+  private counter: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getCounter() {
+    return this.counter;
   }
 
+  increment() {
+    this.counter++;
+  }
 }
